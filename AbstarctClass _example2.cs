c71e56008a77e2699f13fbdd89b3f1b1b33a94f8
PriyankaLohokare <94703042+PriyankaLohokare@users.noreply.github.com>
2021-11-20 21:31:18 +0530

    Add files via upload

---
+++ AbstarctClass _example2.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+abstract class shape
+{
+    public abstract double area();
+}
+class circle:shape
+{
+    double radius;
+    public void setData(double radius)
+    {
+        this.radius = radius;
+
+    }
+    public override double area()
+    {
+        double pi = 3.14;
+        return 3.14 * radius*radius;
+    }
+}
+class rectanle:shape
+{
+    int length, bredth;
+    public void setData(int length,int bredth)
+    {
+        this.length = length;
+        this.bredth = bredth; 
+    }
+    public override double area()
+    {
+        return length * bredth;
+
+    }
+
+}
+class test
+{
+    public static void Main(string[] args)
+    {
+        string choice;
+        double radius;
+        int length;
+        int bredth;
+        circle c=new circle();
+        rectanle r=new rectanle();
+        Console.WriteLine("Enter Your Choice");
+        Console.WriteLine("Rectangle or circle");
+        choice =Console.ReadLine();
+        if (choice.Equals("circle"))
+            
+        {
+            Console.WriteLine("Enter radius");
+            radius =Convert.ToDouble(Console.ReadLine());
+            c.setData(radius);
+            Console.WriteLine("Area of Cicle is");
+            Console.WriteLine(c.area());
+
+        }
+        else if(choice.Equals("rectangle"))
+        {
+            Console.WriteLine("Enter length");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter bredth");
+
+            bredth = Convert.ToInt32(Console.ReadLine());
+
+            r.setData(length,bredth);
+            Console.WriteLine("Area of rectangle is");
+            Console.WriteLine(r.area());
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice!!");
+        }
+        Console.ReadKey();        
+    }
+}
---
+++ AbstractClass_example1.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+abstract class Animal
+{
+    public abstract void Foodhabits();
+    public void behaviour()
+    {
+        Console.WriteLine("All animal eat,walk,make different sounds");
+    }
+
+}
+class herbivore:Animal
+{
+    public override void Foodhabits()
+    {
+        Console.WriteLine("An herbivore" +
+            " is an animal or insect that only eats vegetation, " +
+            "such as grasses, fruits, leaves, vegetables, roots and bulbs");
+    }
+}
+class carnivore : Animal
+{
+    public override void Foodhabits()
+    {
+        Console.WriteLine("A carnivore is type of animal that mostly eats meat, or the flesh of animals");
+    }
+}
+class test
+{
+    public static void Main(string[] args)
+    {
+        herbivore h=new herbivore();
+        carnivore c=new carnivore();
+        h.behaviour();
+        Console.WriteLine("Food Habits of herbivore:");
+        h.Foodhabits();
+        Console.WriteLine("Food Habits of carnivore:");
+        c.Foodhabits();
+        Console.ReadKey();
+    }
+}
