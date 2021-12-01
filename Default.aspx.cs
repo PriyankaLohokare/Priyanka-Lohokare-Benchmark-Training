54ba6fcaae77c15183430f477c1dc24be575d9da
PriyankaLohokare <94703042+PriyankaLohokare@users.noreply.github.com>
2021-12-01 23:24:21 +0530

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //means if page is first time calling then only add value
+        if (!Page.IsPostBack)
+        {
+            ddstate.Items.Add("Maharashtra");
+            ddstate.Items.Add("Goa");
+            ddstate.Items.Add("Madhya Pradesh");
+            ddstate.Items.Add("Gujrat");
+        }
+
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text = "Selected State Is " + ddstate.SelectedItem;
+
+    }
+}
