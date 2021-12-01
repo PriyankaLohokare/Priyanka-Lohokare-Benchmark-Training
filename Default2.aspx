2251ef05fd2d246086b7e94deeda7704205a5989
PriyankaLohokare <94703042+PriyankaLohokare@users.noreply.github.com>
2021-12-01 23:56:41 +0530

    Add files via upload

---
+++ Default2.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body bgcolor="skyblue">
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Default2.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Request.QueryString.Count > 0)
+        {
+            string s = Request.QueryString["data"];
+            Label1.Text = "you selected state in previous page is " + s;
+
+        }
+
+    }
+}
